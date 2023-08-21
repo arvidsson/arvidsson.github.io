8bda943fc715db9a073ef80aaf4c1e02a6ce2220
arvidsson <arvidsson@gmail.com>
2023-08-21 15:19:42 +0200

    restarted from starter project

---
+++ data/metadata.js
@@ -0,0 +1,11 @@
+module.exports = {
+	title: "arvidsson's blog",
+	url: "https://arvidsson.github.io/",
+	language: "en",
+	description: "Blog posts about programming.",
+	author: {
+		name: "Pär Arvidsson",
+		email: "arvidsson@gmail.com",
+		url: "https://arvidsson.github.io/",
+	},
+};
